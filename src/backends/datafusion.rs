e440368ec4f60121d3b2400baae3109a09db7e8f
Tobias Brandt <Tobias.Brandt@gmail.com>
2022-09-26 23:58:56 +0200

    Makes the process_results() function more generalisable

--- src/backends/datafusion.rs
+++ src/backends/datafusion.rs
@@ -1,6 +1,7 @@
 use anyhow::{Result, anyhow};
 use log::{debug, info, warn, error};
 
+use datafusion::arrow::{csv, json};
 use datafusion::arrow::record_batch::RecordBatch;
 use datafusion::arrow::util::pretty::pretty_format_batches;
 
@@ -40,50 +41,10 @@ pub async fn query(query: &str, sources: &SourcesType, to: &ToType, database: &s
     process_results(rbs, to, format)
 }
 
-pub fn process_results(rbs: Vec<RecordBatch>, to: &ToType, format: &str) -> Result<String> {
-
-    let mut buf = Vec::new();
-    let mut output = String::from("");
-    let to = &to.to_string();
-
-    if to != "-" {
-        return Err(anyhow!("Currently only stdout is implemented."))
-    }
-    if format == "table" {
-        output = pretty_format_batches(&rbs)?.to_string();
-    } else if format == "json" {
-        //use datafusion::arrow::json::ArrayWriter;
-        use datafusion::arrow::json::LineDelimitedWriter;
-        let mut writer = LineDelimitedWriter::new(&mut buf);
-        writer.write_batches(&rbs)?;
-        writer.finish()?;
-        output = String::from_utf8(buf)?;
-    } else if format == "csv" {
-        {
-            use datafusion::arrow::csv::Writer;
-            let mut writer = Writer::new(&mut buf);
-            for rb in &rbs {
-                writer.write(&rb)?;
-            }
-        }
-        output = String::from_utf8(buf)?;
-    } else if format == "parquet" {
-        use parquet::arrow::arrow_writer::ArrowWriter;
-        let mut writer = ArrowWriter::try_new(&mut buf, rbs[0].schema(), None)?;
-        for rb in &rbs {
-            writer.write(&rb)?;
-        }
-        writer.close()?;
-        output = String::from_utf8(buf)?;
-    } else {
-        unimplemented!("{to:?}");
-    }
-
-    Ok(output)
-}
-
 async fn process_dataframe(df: DataFrame, to: &ToType) -> Result<String> {
     // Produce the output
+    // This is the easiest method and works fine for DataFusion but is
+    // not portable to the other formats.
     let to = &to.to_string();
     if to == "-" {
         df.show().await?;
@@ -98,4 +59,69 @@ async fn process_dataframe(df: DataFrame, to: &ToType) -> Result<String> {
     }
 
     Ok("".into())
+}
+
+pub fn process_results(rbs: Vec<RecordBatch>, to: &ToType, format: &str) -> Result<String> {
+
+    let mut output = String::from("");
+    let to = &to.to_string();
+
+    if to != "-" {
+        return Err(anyhow!("Currently only stdout is implemented."))
+    }
+
+    if format == "csv" {
+        output = String::from_utf8(convert_record_batches_to_csv(&rbs)?)?;
+    } else if format == "json" {
+        output = String::from_utf8(convert_record_batches_to_json(&rbs)?)?;
+    } else if format == "parquet" {
+        output = String::from_utf8(convert_record_batches_to_parquet(&rbs)?)?;
+    } else if format == "table" {
+        output = pretty_format_batches(&rbs)?.to_string();
+    } else {
+        unimplemented!("{to:?}");
+    }
+
+    Ok(output)
+}
+
+pub fn convert_record_batches_to_csv(rbs: &[RecordBatch]) -> Result<Vec<u8>> {
+    let mut buf = Vec::new();
+    {
+        let mut writer = csv::Writer::new(&mut buf);
+        for rb in rbs {
+            writer.write(rb)?;
+        }
+    }
+    Ok(buf)
+}
+
+pub fn convert_record_batches_to_json(rbs: &[RecordBatch]) -> Result<Vec<u8>> {
+    let mut buf = Vec::new();
+    {
+        // let mut writer = json::ArrayWriter::new(&mut buf);
+        let mut writer = json::LineDelimitedWriter::new(&mut buf);
+        writer.write_batches(&rbs)?;
+        writer.finish()?;
+    }
+    Ok(buf)
+}
+
+pub fn convert_record_batches_to_parquet(rbs: &[RecordBatch]) -> Result<Vec<u8>> {
+    let mut buf = Vec::new();
+
+    if rbs.is_empty() {
+        return Ok(vec![]);
+    }
+
+    let schema = rbs[0].schema();
+    {
+        let mut writer = parquet::arrow::arrow_writer::ArrowWriter::try_new(&mut buf, schema, None)?;
+
+        for rb in rbs {
+            writer.write(rb)?;
+        }
+        writer.close()?;
+    }
+    Ok(buf)
 }
